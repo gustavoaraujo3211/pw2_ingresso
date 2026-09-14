574dea74eec39f94810cf16be5cfa428e19e8be2
Etec Prof. Horácio Augusto da Silveira <gustavoaraujo@gmail.com>
2026-09-14 09:01:13 -0300

    Alteração de classes

--- src/main/java/br/com/etec/ingresso/entity/Filme.java
+++ src/main/java/br/com/etec/ingresso/entity/Filme.java
@@ -3,6 +3,7 @@ package br.com.etec.ingresso.entity;
 import br.com.etec.ingresso.enums.CategoriaFilmeEnum;
 import br.com.etec.ingresso.enums.ClassificacaoIndicativaEnum;
 import br.com.etec.ingresso.enums.SimNaoEnum;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,19 +14,44 @@ import java.time.LocalDateTime;
 @Setter
 @Builder
 @AllArgsConstructor
+@Entity
+@Table(name="TBL_FILME")
 public class Filme {
+    @Id // pk
+    @Column(name = "ID_FILME")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "TX_NOME")
     private String nome;
+    @Column(name = "NR_DURACAO")
     private Integer duracao;
+    @Column(name = "TP_CATEGORIA")
+    @Enumerated(EnumType.STRING)
     private CategoriaFilmeEnum categoria;
+
+    @Column(name = "TP_CLASSIFICACAO")
     private ClassificacaoIndicativaEnum classificacao;
+
+    @Column(name = "NR_ANO")
     private Integer ano;
+
+    @Column(name = "TX_CAPA")
     private String capa;
+
+    @Column(name = "TX_DIRETOR")
     private String diretor;
-    private String elenco;
+
+    @Column(name = "TX_ELENCO")
+    private String elenco;0
+
+    @Column(name = "TX_DESCRICAO")
     private String descricao;
+
+    @Column(name = "NR_AVALIACAO")
     private Double avaliacao;
+
     private SimNaoEnum emCartaz;
+    @Transient
     private LocalDateTime dataExclusao;
 
 }
